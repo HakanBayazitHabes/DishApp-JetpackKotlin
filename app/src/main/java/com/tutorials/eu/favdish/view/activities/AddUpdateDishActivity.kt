4b4377f69f09173d202d8a2f8d2dc5f64cbadc2c
Hakan Bayazıt Habeş <hakanbayazithabes@gmail.com>
2023-08-20 23:14:39 +0300

    Designing the AddUpdateDishesActivity Part 2

--- app/src/main/java/com/tutorials/eu/favdish/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/com/tutorials/eu/favdish/view/activities/AddUpdateDishActivity.kt
@@ -2,10 +2,12 @@ package com.tutorials.eu.favdish.view.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.tutorials.eu.favdish.R
 import com.tutorials.eu.favdish.databinding.ActivityAddUpdateDishBinding
 
-class AddUpdateDishActivity : AppCompatActivity() {
+class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityAddUpdateDishBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +16,9 @@ class AddUpdateDishActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setupActionBar()
+
+        binding.ivAddDishImage.setOnClickListener(this)
+
     }
 
     private fun setupActionBar() {
@@ -21,6 +26,18 @@ class AddUpdateDishActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         binding.toolbarAddDishActivity.setNavigationOnClickListener {
             onBackPressed()
+        }
+    }
+
+    override fun onClick(p0: View?) {
+        if (p0 != null) {
+            when (p0.id) {
+                R.id.iv_add_dish_image -> {
+                    Toast.makeText(this, "You have clicked the ImageView", Toast.LENGTH_SHORT)
+                        .show()
+                    return
+                }
+            }
         }
     }
 }
