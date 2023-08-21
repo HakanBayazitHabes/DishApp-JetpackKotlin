379fbf4c7cd3a1e6ae4692f5ca9caeaa34df2482
Hakan Bayazıt Habeş <hakanbayazithabes@gmail.com>
2023-08-22 01:09:56 +0300

    Setting up the RecyclerView and Adapter using ViewBinding part 1

---
+++ app/src/main/java/com/tutorials/eu/favdish/view/adapters/CustomListItemAdapter.kt
@@ -0,0 +1,33 @@
+package com.tutorials.eu.favdish.view.adapters
+
+import android.app.Activity
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.tutorials.eu.favdish.databinding.ItemCustomListBinding
+
+class CustomListItemAdapter(
+    private val activity: Activity,
+    private val listItems: List<String>,
+    private val selection: String
+) : RecyclerView.Adapter<CustomListItemAdapter.ViewHolder>() {
+
+    class ViewHolder(view: ItemCustomListBinding) : RecyclerView.ViewHolder(view.root) {
+        val tvText = view.tvText
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding: ItemCustomListBinding =
+            ItemCustomListBinding.inflate(activity.layoutInflater, parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return listItems.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = listItems[position]
+        holder.tvText.text = item
+    }
+
+}
