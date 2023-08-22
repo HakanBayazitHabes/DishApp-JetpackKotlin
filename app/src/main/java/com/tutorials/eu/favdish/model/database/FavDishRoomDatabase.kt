036377bef66dc4a81bea8ae5a3ff3d2406217f68
Hakan Bayazıt Habeş <hakanbayazithabes@gmail.com>
2023-08-22 06:01:13 +0300

    Setting up the ROOM database

---
+++ app/src/main/java/com/tutorials/eu/favdish/model/database/FavDishRoomDatabase.kt
@@ -0,0 +1,37 @@
+package com.tutorials.eu.favdish.model.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.tutorials.eu.favdish.model.entities.FavDish
+
+@Database(entities = [FavDish::class], version = 1)
+abstract class FavDishRoomDatabase : RoomDatabase() {
+    companion object {
+        @Volatile
+        private var INSTANCE: FavDishRoomDatabase? = null
+
+        fun getDatabase(context: Context): FavDishRoomDatabase {
+            val tempInstance = INSTANCE
+            if (tempInstance != null) {
+                return tempInstance
+            }
+
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    FavDishRoomDatabase::class.java,
+                    "fav_dishes_database"
+                )
+                    .fallbackToDestructiveMigration()
+                    .build()
+
+                INSTANCE = instance
+                return instance
+            }
+        }
+
+
+    }
+}
