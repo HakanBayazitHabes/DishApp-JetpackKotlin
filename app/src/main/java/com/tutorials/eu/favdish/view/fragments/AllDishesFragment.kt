9cbcbac9bc9de673ac7131c0737c089dcf1e93ff
Hakan Bayazıt Habeş <hakanbayazithabes@gmail.com>
2023-08-22 16:34:18 +0300

    Retrieving the inserted data from the database

--- app/src/main/java/com/tutorials/eu/favdish/view/fragments/AllDishesFragment.kt
+++ app/src/main/java/com/tutorials/eu/favdish/view/fragments/AllDishesFragment.kt
@@ -2,6 +2,7 @@ package com.tutorials.eu.favdish.view.fragments
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuInflater
@@ -10,15 +11,24 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.tutorials.eu.favdish.R
+import com.tutorials.eu.favdish.application.FavDishApplication
 import com.tutorials.eu.favdish.view.activities.AddUpdateDishActivity
+import com.tutorials.eu.favdish.viewmodel.FavDishViewModel
+import com.tutorials.eu.favdish.viewmodel.FavDishViewModelFactory
 import com.tutorials.eu.favdish.viewmodel.HomeViewModel
 
 class AllDishesFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
+
+    private val mFavDishViewModel: FavDishViewModel by viewModels {
+        FavDishViewModelFactory((requireActivity().application as FavDishApplication).repository)
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,12 +36,12 @@ class AllDishesFragment : Fragment() {
     }
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         homeViewModel =
-                ViewModelProvider(this).get(HomeViewModel::class.java)
+            ViewModelProvider(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_all_dishes, container, false)
         val textView: TextView = root.findViewById(R.id.text_home)
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
@@ -40,13 +50,25 @@ class AllDishesFragment : Fragment() {
         return root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mFavDishViewModel.allDishesList.observe(viewLifecycleOwner) { dishes ->
+            dishes.let {
+                for (item in it) {
+                    Log.i("Dish Title", "${item.id} :: ${item.title}")
+                }
+            }
+        }
+    }
+
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.menu_all_dishes, menu)
         super.onCreateOptionsMenu(menu, inflater)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId) {
+        when (item.itemId) {
             R.id.action_add_dish -> {
                 startActivity(Intent(requireActivity(), AddUpdateDishActivity::class.java))
                 return true
