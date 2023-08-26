351bf6002ab182d0c2495b02ac942a0e4fff7ba4
Hakan Bayazıt Habeş <hakanbayazithabes@gmail.com>
2023-08-27 02:50:19 +0300

    Setting up the dish details fragment

--- app/src/main/java/com/tutorials/eu/favdish/view/fragments/DishDetailsFragment.kt
+++ app/src/main/java/com/tutorials/eu/favdish/view/fragments/DishDetailsFragment.kt
@@ -7,10 +7,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
 import com.tutorials.eu.favdish.R
+import com.tutorials.eu.favdish.databinding.FragmentDishDetailsBinding
+import java.util.Locale
 
 
 class DishDetailsFragment : Fragment() {
+
+    private var binding: FragmentDishDetailsBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,16 +26,39 @@ class DishDetailsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentDishDetailsBinding.inflate(inflater, container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dish_details, container, false)
+        return binding!!.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val args: DishDetailsFragmentArgs by navArgs()
-        Log.i("Dish title", args.dishDetails.title)
-        Log.i("Dish type", args.dishDetails.type)
 
+        args.let {
+            try {
+                Glide.with(requireActivity())
+                    .load(it.dishDetails.image)
+                    .centerCrop()
+                    .into(binding!!.ivDishImage)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+
+            binding!!.tvTitle.text = it.dishDetails.title
+            binding!!.tvType.text = it.dishDetails.type.capitalize(Locale.ROOT)
+            binding!!.tvCategory.text = it.dishDetails.category
+            binding!!.tvIngredients.text = it.dishDetails.ingredients
+            binding!!.tvCookingDirection.text = it.dishDetails.directionToCook
+            binding!!.tvCookingTime.text =
+                resources.getString(R.string.lbl_estimate_cooking_time, it.dishDetails.cookingTime)
+        }
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
     }
 
 }
